ab8e21bc12da3c5a7509fb246ea2360aa90aafcc
Nazim Saifi <saifinazim2311@gmail.com>
2025-10-31 22:40:06 +0530

    init

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+const app=express()
+
+
+const PORT=process.env.PORT || 9000
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+app.listen(PORT,()=>console.log("Server is running at Port :",PORT))
