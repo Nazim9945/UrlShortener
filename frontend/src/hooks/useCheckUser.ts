86eb4bcf67eef394101af416e6de82ad9d53f98c
Nazim Saifi <saifinazim2311@gmail.com>
2025-11-17 01:21:59 +0530

    install react-query

--- frontend/src/hooks/useCheckUser.ts
+++ frontend/src/hooks/useCheckUser.ts
@@ -1,26 +1,41 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { axiosInstance } from "../helper/axiosInstance";
 import { useAuth } from "../context/store";
+import {  useQuery } from "@tanstack/react-query";
 
 const useCheckUser = () => {
   const { user, isAuth } = useAuth();
-  const [isLoading, setLoading] = useState(false);
-  const meProfile = async () => {
-    setLoading(true);
-    try {
-      const res = await axiosInstance.get("api/auth/me");
-      isAuth(res.data.user);
-    } catch (err) {
-      console.error(err);
-      isAuth(null);
-    } finally {
-      setLoading(false);
+  // const [isLoading, setLoading] = useState(false);
+  const {isLoading}=useQuery({
+    queryKey:['isLoggedInUser'],
+    queryFn:()=>{
+       axiosInstance.get("/api/auth/me").then(res=>{
+         console.log(res);
+         isAuth(res.data)
+         return res.data
+       });
+      
     }
-  };
+  })
+  
+  
+ 
+  // const meProfile = async () => {
+  //   setLoading(true);
+  //   try {
+  //     const res = await axiosInstance.get("api/auth/me");
+  //     isAuth(res.data.user);
+  //   } catch (err) {
+  //     console.error(err);
+  //     isAuth(null);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    meProfile();
-  }, []);
+  // useEffect(() => {
+  //   meProfile();
+  // }, []);
   return { isLoading, user };
 };
 export default useCheckUser;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,15 +3,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router";
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 
-
+const queryClient=new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  
-    <BrowserRouter>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
       <App />
       <Toaster position="top-center" reverseOrder={false} />
-    </BrowserRouter>
- 
+    </QueryClientProvider>
+  </BrowserRouter>
 );
 
